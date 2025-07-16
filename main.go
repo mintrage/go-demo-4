b7da7fc5e389aa56b37982eb9ae79b4b8d226d71
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-16 16:29:07 +0300

    8.3. Создание указателя Указатели

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	a := 5
+	pointerA := &a
+	res := double(a)
+	fmt.Println(pointerA)
+	fmt.Println(res)
+}
+
+func double(num int) int {
+	return num * 2
+}
