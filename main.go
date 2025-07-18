7bb63398e11650a6092d4633e491fc03464a472d
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-18 16:02:30 +0300

    9.1. Зачем нужны Struct Struct

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	outputPassword(login, password, url)
+
 }
 
-func reverse(arr *[4]int) {
-	for index, value := range *arr {
-		(*arr)[len(arr)-1-index] = value
-	}
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
 
+func outputPassword(login, password, url string) {
+	fmt.Println(login, password, url)
 }
