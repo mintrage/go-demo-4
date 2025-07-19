40344a31d50f9441a5b4663881d68af8c9419de6
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-19 13:50:31 +0300

    10.1. Разделение кода Пакеты

--- main.go
+++ main.go
@@ -1,83 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
-	"time"
 )
-
-type account struct {
-	login    string
-	password string
-	url      string
-}
-
-type accountWithTimeStamp struct {
-	createdAt time.Time
-	updatedAt time.Time
-	acc       account
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	acc.password = string(res)
-}
-
-// 1. Если логина нет, то ошибка
-// 2. Если нет пароля, то генерим
-func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
-	if login == "" {
-		return nil, errors.New("INVALID_LOGIN")
-	}
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL")
-	}
-	newAcc := &accountWithTimeStamp{
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
-		acc: account{
-			login:    login,
-			password: password,
-			url:      urlString,
-		},
-	}
-	if password == "" {
-		newAcc.acc.generatePassword(12)
-	}
-	return newAcc, err
-}
-
-// func newAccount(login, password, urlString string) (*account, error) {
-// 	if login == "" {
-// 		return nil, errors.New("INVALID_LOGIN")
-// 	}
-// 	_, err := url.ParseRequestURI(urlString)
-// 	if err != nil {
-// 		return nil, errors.New("INVALID_URL")
-// 	}
-// 	newAcc := &account{
-// 		login:    login,
-// 		password: password,
-// 		url:      urlString,
-// 	}
-// 	if password == "" {
-// 		newAcc.generatePassword(12)
-// 	}
-// 	return newAcc, err
-// }
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
 	// fmt.Println(generatePassword(12))
