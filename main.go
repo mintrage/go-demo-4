bdeaf2cb9156aa20edc967c4e7f638958fa96ec9
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-07-26 00:03:14 +0300

    13.1. Тип функции Продвинутые функции

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/fatih/color"
 )
 
+var menu = map[string]func(*account.VaultWithDb){
+	"1": createAccount,
+	"2": findAccount,
+	"3": deleteAccount,
+}
+
 func main() {
 	// output.PrintError(1)
 	// output.PrintError("sd")
@@ -24,16 +30,20 @@ Menu:
 			"4. Выход",
 			"Выберите вариант",
 		})
-		switch variant {
-		case "1":
-			createAccount(vault)
-		case "2":
-			findAccount(vault)
-		case "3":
-			deleteAccount(vault)
-		default:
+		menuFunc := menu[variant]
+		if menuFunc == nil {
 			break Menu
 		}
+		menuFunc(vault)
+		// switch variant {
+		// case "1":
+		// 	createAccount(vault)
+		// case "2":
+		// 	findAccount(vault)
+		// case "3":
+		// 	deleteAccount(vault)
+		// default:
+		// 	break Menu
 	}
 }
 
